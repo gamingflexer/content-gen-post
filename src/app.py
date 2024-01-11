2c9decfb2fbf57251e48537c26a6fd15744ef78e
gamingflexer <osurve20ece@student.mes.ac.in>
2024-01-11 18:45:53 +0000

    Add PDF file upload functionality for rephrasing

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 import gradio as gr
 import time
-from uuid import uuid4
+from srcapper.summariser import daily_news_summary, summarized_daily_news_blog, daily_post_single, weekly_news_summary, weekly_news_blog, rephrase
 from content_maker import scrapper_content,get_news_content
+from utils import extract_text_from_pdf
 
-def load_dataset_local():
-    time.sleep(10)
     
 def daily_post_tqdm(new_exp_name, progress=gr.Progress()):
     progress(0.2, desc="Collecting Links")
@@ -37,14 +36,23 @@ def weekly_summarised_post_tqdm(exp_name, progress=gr.Progress()):
     time.sleep(1.5)
     return "done"
 
-def pdf_rephrasing_tqdm(exp_name, progress=gr.Progress()):
+def pdf_rephrasing_tqdm(files, progress=gr.Progress()):
+    file_paths = [file.name for file in files]
+    total_text = ""
+    for file_path in file_paths:
+      text = extract_text_from_pdf(file_path)
+      if text is not None:
+        total_text = total_text + " " + text
+    
+    blog_pdf = rephrase(total_text)
+    print(file_paths)
     progress(0.2, desc="Collecting Images")
     time.sleep(1)
     progress(0.5, desc="Cleaning Images")
     time.sleep(1.5)
     progress(0.8, desc="Sending Images")
     time.sleep(1.5)
-    return "done"
+    return blog_pdf
 
 with gr.Blocks() as demo:
         
@@ -72,15 +80,12 @@ with gr.Blocks() as demo:
         
     with gr.Tab("Pdf article Rephrasing"):
         with gr.Row():
-            pdf_files_upload_2 = gr.File(label="Upload PDF files")
-            start_button_4 = gr.Button("Start")
-        loading_bar_text_4 = gr.Textbox(label="PreProcess Loading Bar")
-        with gr.Row():
-          download_button_4 = gr.Button("Download")
+          file_output = gr.File()
+          pdf_files_upload_2 = gr.UploadButton("Click to Upload a File", file_types=["pdf"], file_count="multiple")
         
     start_button_1.click(daily_post_tqdm, outputs=loading_bar_text_1)
     start_button_2.click(daily_summarised_post_tqdm, outputs=loading_bar_text_2)
     start_button_3.click(weekly_summarised_post_tqdm, outputs=loading_bar_text_3)
-    start_button_4.click(pdf_rephrasing_tqdm, outputs=loading_bar_text_4)
+    pdf_files_upload_2.upload(pdf_rephrasing_tqdm, pdf_files_upload_2, file_output)
 
 demo.launch(server_name='0.0.0.0', server_port=7866)
